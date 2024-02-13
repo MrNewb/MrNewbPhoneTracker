daae09521684ebe6bd16e60955c28482035bd8cf
Donivan <donivansee@live.com>
2024-02-13 01:54:55 -0500

    Updated qs compatability, new strings for it in config.lua.
    This is setup for new qs-smartphone to search the entire database for this. It was the only way possible at this time to do it. OldQsPhone is now compatible, I didnt know till recently it saves to playerdata like the standard phone.  --- let me know if you run into issues.
    
    Thank you Quasar for sending me the phone so I could add compatibility easier and test.

---
+++ bridge/[phones]/old-qs-phone/server.lua
@@ -0,0 +1,15 @@
+if Config.Phone ~= "OldQsPhone" then return print("OldQsPhone SS Off") end
+
+function getPlayerCoords(TargetSource)
+	if not TargetSource then return nil end
+    local TargetPed = GetPlayerPed(TargetSource)
+    local coords = GetEntityCoords(TargetPed)
+    return coords
+end
+
+function getPlayerSource(phoneNumber)
+    if not phoneNumber then return nil end
+	local TargetSource = QBCore.Functions.GetPlayerByPhone(phoneNumber)
+	if not TargetSource then return nil end
+	return TargetSource.PlayerData.source
+end
--- bridge/[phones]/qs-phone/server.lua
+++ bridge/[phones]/qs-phone/server.lua
@@ -1,31 +1,33 @@
-if Config.Phone ~= "QsPhone" then return print("QsPhone SS Off") end
+if Config.Phone ~= "NewQsPhone" then return print("NewQsPhone SS Off") end
 
-local function getOwnerByPhoneNumber(phoneNumber)	
-	local response = MySQL.query.await('SELECT `owner` FROM `phone_backups` WHERE `phone` = ?;', { phoneNumber })
+local function getOwnerByPhoneNumber(phoneNumber)
+    local response = MySQL.query.await('SELECT `citizenid` FROM `players` WHERE `inventory` LIKE \'%"phone":"' .. phoneNumber .. '"%\'', {})
 
-	if response and #response > 0 then 
-		return response[1].owner 
-	end
-
+    if response and #response > 0 then
+        return response[1].citizenid
+    end
+	
 	return nil
 
 end
 
-local function getPlayerDataByCitizenId(CitizenID)
-    local CitizenID = QBCore.Functions.GetPlayerByCitizenId(citizenId)
-    if not CitizenID then return nil end
-	return CitizenID.PlayerData.source
+local function getPlayerDataByCitizenId(citizenId)
+    local Player = QBCore.Functions.GetPlayerByCitizenId(citizenId)
+    if not Player then return nil end
+    return Player.PlayerData.source
 end
 
 function getPlayerSource(phoneNumber)
-	local CitizenID = getOwnerByPhoneNumber(phoneNumber)
-	local TargetSource = getPlayerDataByCitizenId(CitizenID)
-	if not TargetSource then return nil end
-	return TargetSource
+	if not phoneNumber then return nil end
+    local citizenId = getOwnerByPhoneNumber(phoneNumber)
+    if not citizenId then return nil end
+    local src = getPlayerDataByCitizenId(citizenId)
+    return src
 end
 
-function getPlayerCoords(TargetSource)
-	local ped = GetPlayerPed(TargetSource)
-	local coords = GetEntityCoords(ped)
-	return coords
+function getPlayerCoords(source)
+	if not source then return nil end
+    local ped = GetPlayerPed(source)
+    local coords = GetEntityCoords(ped)
+    return coords
 end
