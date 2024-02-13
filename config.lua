1c45347298aec9fdeca14d886fa0e99423e62d49
ARottenberger <69944020+iplayer1337fivem@users.noreply.github.com>
2024-02-13 02:00:12 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,6 +4,6 @@ Config = {
 	Logs = "QbLogs",											-- set this to the name of the method, pass as string. options are "QbLogs","OxLogs", set to false to disable
 	Notify = "OxNotify",										-- set this to the name of the method, pass as string. options are "OxNotify","QBNotify","OKNotify","SDNotify"
 	Input = "OxInput",											-- set this to the name of the method, pass as string. options are "OxInput","QbInput"
-	Phone = "NPWD",												-- set this to the name of the method, pass as string. options are "NPWD","lbphone","QbPhone","QsPhone","gks"
+	Phone = "NPWD",												-- set this to the name of the method, pass as string. options are "NPWD","lbphone","QbPhone","QsPhone","gks", "yflip"
 	Alert = "PsDispatch",										-- set this to the name of the method, pass as string. options are "PsDispatch","Standalone","cd_Dispatch","qs_Dispatch"
 }
