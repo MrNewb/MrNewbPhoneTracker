605eb6d96065014d4506bd7602a160df622dd215
Donivan <donivansee@live.com>
2024-02-21 05:40:19 -0500

    Jp phone compatibility (qb)

---
+++ bridge/[phones]/jp-phone/server.lua
@@ -0,0 +1,15 @@
+if Config.Phone ~= "jpPhone" then return print("jpPhone SS Off") end
+
+function getPlayerCoords(TargetSource)
+	if not TargetSource then return nil end
+    local TargetPed = GetPlayerPed(TargetSource)
+    local coords = GetEntityCoords(TargetPed)
+    return coords
+end
+
+function getPlayerSource(phoneNumber)
+    if not phoneNumber then return nil end
+	local TargetSource = QBCore.Functions.GetPlayerByPhone(phoneNumber)
+	if not TargetSource then return nil end
+	return TargetSource.PlayerData.source
+end
